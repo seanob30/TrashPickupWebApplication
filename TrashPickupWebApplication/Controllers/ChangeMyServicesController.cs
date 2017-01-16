41b2846c0c32a614e284c04e4116d6bcf1470133
Grant Smith <smithge87@gmail.com>
2017-01-16 09:49:34 -0600

    MyServices buttons working, adding controller to deal with data

---
+++ TrashPickupWebApplication/Controllers/ChangeMyServicesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashPickupWebApplication.Controllers
+{
+    public class ChangeMyServicesController : Controller
+    {
+        // GET: ChangeMyServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // HTTP: POST : ChangeMyServices
+
+    }
+}
