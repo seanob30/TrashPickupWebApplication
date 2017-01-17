efc2c55fc6bc1dfb2109a2307fd0428ffae77bd7
Grant Smith <smithge87@gmail.com>
2017-01-17 09:51:52 -0600

    CustomerInfo Registration page added

---
+++ TrashPickupWebApplication/Controllers/RegistrationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashPickupWebApplication.Models;
+
+namespace TrashPickupWebApplication.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        ApplicationDbContext _context;
+        public RegistrationController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContactInfo()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ContactInfo(UserInfo model)
+        {
+            model.SignUpDate = DateTime.Now.ToString();
+            _context.UserInfo.Add(model);
+            return RedirectToAction("ServicesInfo", "Registration");
+        }
+    }
+}
