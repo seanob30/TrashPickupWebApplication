61702553af3b094d66f7d68983c9d80afa255e74
Grant Smith <smithge87@gmail.com>
2017-01-18 16:31:17 -0600

    added paymentviewmodel content

--- TrashPickupWebApplication/ViewModels/PaymentViewModel.cs
+++ TrashPickupWebApplication/ViewModels/PaymentViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,35 @@ namespace TrashPickupWebApplication.Models
 {
     public class PaymentViewModel
     {
+        [Required]
+        [Display(Name = "Card Type")]
+        public int CardTypeId { get; set; }
 
+        [Required]
+        [Display(Name = "Card Number")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Expiration Month")]
+        public int ExpirationMonth { get; set; }
+
+        [Required]
+        [Display(Name = "Expiration Year")]
+        public int ExpirationYear { get; set; }
+
+        [Required]
+        [Display(Name = "CCV Code")]
+        public int CCVCode { get; set; }
+
+        [Required]
+        [Display(Name = "Name on Card")]
+        public string NameOnCard { get; set; }
+
+        [Required]
+        [Display(Name = "Zipcode on Card")]
+        public int ZipcodeOnCard { get; set; }
+
+        public IEnumerable<PaymentInformation> PaymentInformationList { get; set; }
+        public IEnumerable<CardTypes> CardTypesList { get; set; }
     }
 }
