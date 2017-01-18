af1a5994773979e117bfc335a7e4b7ce1dc2d91b
Grant Smith <smithge87@gmail.com>
2017-01-18 12:59:04 -0600

    Reseeded the rest of the static data

--- TrashPickupWebApplication/Migrations/Configuration.cs
+++ TrashPickupWebApplication/Migrations/Configuration.cs
@@ -23,6 +23,40 @@ namespace TrashPickupWebApplication.Migrations
                  new Models.Days { ID = 5, Day = "Friday" },
                  new Models.Days { ID = 6, Day = "Saturday" }
                  );
+            context.RegularServices.AddOrUpdate(m => m.ID,
+                new Models.RegularServices { ID = 1, ServiceName = "Trash Pickup", CostPerPickup = 15 },
+                new Models.RegularServices { ID = 1, ServiceName = "Recycling Pickup", CostPerPickup = 20 }
+                );
+            context.PickupIntervals.AddOrUpdate(m => m.ID,
+                new Models.PickupIntervals { ID = 1, Frequency = "Once" },
+                new Models.PickupIntervals { ID = 1, Frequency = "Every Day" },
+                new Models.PickupIntervals { ID = 1, Frequency = "Once A Week" },
+                new Models.PickupIntervals { ID = 1, Frequency = "Twice a month" },
+                new Models.PickupIntervals { ID = 1, Frequency = "Once A Month" }
+                );
+            context.ZipCode.AddOrUpdate(m => m.ID,
+                new Models.ZipCode { ID = 4, Zipcodes = 53209 },
+                new Models.ZipCode { ID = 5, Zipcodes = 53225 },
+                new Models.ZipCode { ID = 6, Zipcodes = 53216 },
+                new Models.ZipCode { ID = 7, Zipcodes = 53222 },
+                new Models.ZipCode { ID = 8, Zipcodes = 53005 },
+                new Models.ZipCode { ID = 9, Zipcodes = 53226 },
+                new Models.ZipCode { ID = 10, Zipcodes = 53208 },
+                new Models.ZipCode { ID = 11, Zipcodes = 53214 },
+                new Models.ZipCode { ID = 12, Zipcodes = 53215 },
+                new Models.ZipCode { ID = 13, Zipcodes = 53227 },
+                new Models.ZipCode { ID = 14, Zipcodes = 53122 }
+                );
+            context.State.AddOrUpdate(m => m.ID,
+                new Models.State { ID = 2, States = "Illinois" },
+                new Models.State { ID = 3, States = "Indiania" },
+                new Models.State { ID = 4, States = "Minnesota" },
+                new Models.State { ID = 5, States = "Iowa" },
+                new Models.State { ID = 6, States = "Ohio" },
+                new Models.State { ID = 7, States = "Kansas" },
+                new Models.State { ID = 8, States = "Missouri" },
+                new Models.State { ID = 9, States = "North Dakota" }
+                );
         }
     }
 }
