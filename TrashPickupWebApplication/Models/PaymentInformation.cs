9c81661b2180c14fdf47551fc01a027406d542b3
Grant Smith <smithge87@gmail.com>
2017-01-18 15:32:41 -0600

    added paymentInfo and card types models

---
+++ TrashPickupWebApplication/Models/PaymentInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashPickupWebApplication.Models
+{
+    public class PaymentInformation
+    {
+        [Key]
+        public int ID { get; set; }
+        public CardTypes CardType { get; set; }
+        public int CardTypeId { get; set; }
+        public int CardNumber { get; set; }
+        public int ExpirationMonth { get; set; }
+        public int ExpirationYear { get; set; }
+        public int CCVCode { get; set; }
+        public string NameOnCard { get; set; }
+        public int ZipcodeOnCard { get; set; }
+    }
+}
