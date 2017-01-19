f75588c572f296896e87c8655edb47268f985114
Grant Smith <smithge87@gmail.com>
2017-01-19 08:05:09 -0600

    added zip code selector to myRoutes and a MyRoutesViewModel

--- TrashPickupWebApplication/Controllers/MyRoutesController.cs
+++ TrashPickupWebApplication/Controllers/MyRoutesController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashPickupWebApplication.Models;
 
 namespace TrashPickupWebApplication.Controllers
 {
     public class MyRoutesController : Controller
     {
+        public ApplicationDbContext _context;
+        public MyRoutesController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: MyRoutes
         public ActionResult Index()
         {
-            return View();
+            var zipcodes = _context.ZipCode.ToList();
+
+            var viewModel = new MyRoutesViewModel()
+            {
+                ZipCodeList = zipcodes,
+            };
+            return View(viewModel);
         }
     }
 }
---
+++ TrashPickupWebApplication/ViewModels/MyRoutesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TrashPickupWebApplication.Models;
+
+namespace TrashPickupWebApplication.Models
+{
+    public class MyRoutesViewModel
+    {
+        [Required]
+        [Display(Name = "Zipcode")]
+        public string ZipCodeId { get; set; }
+
+
+        public IEnumerable <ZipCode> ZipCodeList { get; set; }
+        public IEnumerable<ApplicationUser> ApplicationUserList { get; set; }
+    }
+}
